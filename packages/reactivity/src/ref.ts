f1a0c4045850befe1db89178d2f4193d111ab2f2
Linzsong <744740666@qq.com>
2025-07-23 23:55:35 +0800

    feat: 添加链表-1

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -5,6 +5,18 @@ enum ReactiveFlags {
 }
 
 /**
+ * 链表节点
+ */
+interface Link {
+  // 保存 effect
+  sub: Function;
+  // 下一个节点
+  nextSub: Link | undefined;
+  // 上一个节点
+  prevSub: Link | undefined;
+}
+
+/**
  * Ref 类
  */
 class RefImpl {
@@ -13,8 +25,15 @@ class RefImpl {
   // ref 标记，证明是一个 ref
   [ReactiveFlags.IS_REF] = true;
 
-  // 保存与 effect 的关联关系
-  subs;
+  /**
+   * 订阅者链表的头节点
+   */
+  subs: Link;
+
+  /**
+   * 订阅者链表的尾节点
+   */
+  subsTail: Link;
 
   constructor(value) {
     this._value = value;
@@ -24,6 +43,25 @@ class RefImpl {
     if (activeSub) {
       // 如果 activeSub 存在，则保存起来，等到更新时重新出发
       this.subs = activeSub;
+      const newLink: Link = {
+        sub: activeSub,
+        nextSub: undefined,
+        prevSub: undefined,
+      };
+
+      /**
+       * 关联链表关系，分2种情况
+       * 1. 有尾节点，就往尾节点后面添加
+       * 2. 如果没有尾节点，则表示第一次关联，头尾节点相同
+       */
+      if (this.subsTail) {
+        this.subsTail.nextSub = newLink;
+        newLink.prevSub = this.subsTail;
+        // this.subsTail = newLink
+      } else {
+        this.subs = newLink
+      }
+      this.subsTail = newLink
     }
     return this._value;
   }
@@ -32,7 +70,12 @@ class RefImpl {
     this._value = newValue;
 
     // 通知 effect 重新执行, 获取到最新的值
-    this.subs();
+    let currentSub = this.subs
+    currentSub.sub();
+    while (currentSub.nextSub) {
+      currentSub = currentSub.nextSub
+      currentSub.sub();
+    }
   }
 }
 
